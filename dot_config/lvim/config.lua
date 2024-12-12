0ca090a43f734bb616e097b28e76ae4db56ec9c4
JYYYeung <yeungjyy@gmail.com>
2024-12-12 23:04:13 +0800

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -3,3 +3,14 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+--
+lvim.plugins = {
+	{
+	  "tpope/vim-surround",
+
+	  -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+	  -- setup = function()
+	    --  vim.o.timeoutlen = 500
+	  -- end
+	},
+}
